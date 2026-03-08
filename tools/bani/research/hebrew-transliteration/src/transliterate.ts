f08c06bb5937614784fd5b89dc9a6d2ca9755f95
Jhonny <edy602425@gmail.com>
2026-03-08 18:09:01 -0300

    fix: convert hebrew-transliteration to regular directory for cloudflare builds

---
+++ tools/bani/research/hebrew-transliteration/src/transliterate.ts
@@ -0,0 +1,106 @@
+import { Text } from "havarotjs";
+import { SylOpts } from "havarotjs/text";
+import { Word } from "havarotjs/word";
+import { sylRules, wordRules } from "./rules";
+import { SBL, Schema } from "./schema";
+
+/**
+ *  Gets the syllable options from a partial schema
+ *
+ * @private
+ * @param schema
+ * @returns syllable options passed into havarotjs
+ *
+ * @remarks
+ * Sanitizes the SylOpts of the schema so as to not pass in undefined
+ */
+const getSylOpts = (schema: Partial<Schema>): SylOpts => {
+  return {
+    allowNoNiqqud: schema.allowNoNiqqud,
+    article: schema.article,
+    holemHaser: schema.holemHaser,
+    ketivQeres: schema.ketivQeres,
+    longVowels: schema.longVowels,
+    qametsQatan: schema.qametsQatan,
+    shevaAfterMeteg: schema.shevaAfterMeteg,
+    shevaWithMeteg: schema.shevaWithMeteg,
+    sqnmlvy: schema.sqnmlvy,
+    strict: schema.strict,
+    wawShureq: schema.wawShureq
+  };
+};
+
+/**
+ * Transliterates Hebrew text according to a given schema
+ *
+ * @param text - a string or {@link https://charlesloder.github.io/havarotjs/classes/text.Text.html | Text} of Hebrew characters
+ * @param schema - a {@link Schema} for transliterating the text
+ * @returns a transliterated text
+ *
+ * @example
+ * Default
+ * ```ts
+ * import { transliterate } from "hebrew-transliteration";
+ *
+ * transliterate("אֱלֹהִים");
+ * // "ʾĕlōhîm";
+ * ```
+ *
+ * @example
+ * Using `Partial<Schema>`
+ * ```ts
+ * import { transliterate } from "hebrew-transliteration";
+ *
+ * transliterate("שָׁלוֹם", { SHIN: "sh" })
+ * // shālôm
+ * ```
+ *
+ * @example
+ * Using a custom `Schema`
+ * ```ts
+ * import { transliterate, Schema } from "hebrew-transliteration";
+ *
+ * const schema = new Schema({ ALEF: "'", BET: "B", ... QAMETS: "A", ... }) // truncated for brevity
+ *
+ * transliterate("אָ֣ב", schema)
+ * // 'AB
+ * ```
+ *
+ * @remarks
+ * If no {@link Schema} is passed, then the package defaults to SBL's academic style. You can pass in a partial schema that will modify SBL's academic style.
+ * If you need a fully custom schema, it is best to use the {@link Schema} constructor.
+ *
+ */
+export const transliterate = (text: string | Text, schema?: Partial<Schema> | Schema) => {
+  const transSchema = schema instanceof Schema ? schema : new SBL(schema ?? {});
+  const newText = text instanceof Text ? text : new Text(text, getSylOpts(transSchema ?? {}));
+  return newText.words
+    .map((word) => {
+      const transliteration = wordRules(word, transSchema);
+
+      if (!(transliteration instanceof Word)) {
+        return `${transliteration}${word.whiteSpaceAfter ?? ""}`;
+      }
+
+      const syllableTransliteration = transliteration.syllables
+        .map((s) => sylRules(s, transSchema))
+        .reduce((a, c, i) => {
+          if (!i) {
+            return a + c;
+          }
+
+          if (!transSchema.SYLLABLE_SEPARATOR) {
+            return a + c;
+          }
+
+          if (c.includes(transSchema.SYLLABLE_SEPARATOR)) {
+            return a + c;
+          }
+
+          return a + transSchema.SYLLABLE_SEPARATOR + c;
+        }, "");
+
+      return `${syllableTransliteration}${word.whiteSpaceAfter ?? ""}`;
+    })
+    .join("");
+};
---
+++ tools/bani/research/hebrew-transliteration/test/schemas/romaniote.test.ts
@@ -0,0 +1,147 @@
+import { transliterate } from "../../src/index";
+import { romaniote } from "../../src/schemas/romaniote";
+import { describe, expect, test } from "vitest";
+
+describe.each`
+  description              | hebrew       | transliteration
+  ${"spirantized bet"}     | ${"אָב"}     | ${"αβ"}
+  ${"unspirantized bet"}   | ${"בָּם"}    | ${"μπαμ"}
+  ${"spirantized gimel"}   | ${"חָג"}     | ${"χαγ"}
+  ${"unspirantized gimel"} | ${"גָּדַל"}  | ${"γκαδάλ"}
+  ${"spirantized dalet"}   | ${"סַד"}     | ${"σαδ"}
+  ${"unspirantized dalet"} | ${"דָּם"}    | ${"νταμ"}
+  ${"spirantized zayin"}   | ${"הַזֹּאת"} | ${"ατζώθ"}
+  ${"unspirantized zayin"} | ${"זֵה"}     | ${"ζε"}
+  ${"spirantized kaf"}     | ${"לֵךְ"}    | ${"λεχ"}
+  ${"unspirantized kaf"}   | ${"כָּמָר"}  | ${"καμάρ"}
+  ${"spirantized peh"}     | ${"אֶלֶף"}   | ${"ελέφ"}
+  ${"unspirantized peh"}   | ${"פֶּה"}    | ${"πε"}
+  ${"spirantized tav"}     | ${"מַת"}     | ${"μαθ"}
+  ${"unspirantized tav"}   | ${"תָּם"}    | ${"ταμ"}
+`("Spirantization:", ({ hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${hebrew} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description                  | hebrew      | transliteration
+  ${"shin char plus shin dot"} | ${"שֶׁלֶם"}  | ${"σσελέμ"}
+  ${"shin char plus sin dot"}  | ${"אָרַשׂ"}  | ${"αράς"}
+  ${"final sigmas"}            | ${"לָבֵשׁ"} | ${"λαβέσς"}
+`("Sibiliants:", ({ hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${hebrew} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description                 | hebrew                             | transliteration
+  ${"no special features"}    | ${"אָמַ֣ר"}                        | ${"αμάρ"}
+  ${"gemination"}             | ${"רַנֵּן"}                        | ${"ρανέν"}
+  ${"divine name"}            | ${"יְהוָ֣ה"}                       | ${"Αδωνάη"}
+  ${"furtive patach, chet"}   | ${"שָׂמֵחַ"}                        | ${"σαμεάχ"}
+  ${"furtive patach, ayin"}   | ${"שָׁמֵעַ"}                       | ${"σσαμεά"}
+  ${"furtive patach, he"}     | ${"גָבֹהַּ"}                       | ${"γαβωά"}
+  ${"3ms suffix"}             | ${"דְּבָרָ֖יו"}                    | ${"ντεβαράβ"}
+  ${"mixed with latin chars"} | ${"רוּחַ, אֲבֹותֵינוּ (לְעֹולָם)"} | ${"ρουαχ, αβωθενού (λεωλαμ)"}
+  ${"qamets qatan"}           | ${"כָּל הָעוֹלָם כָּל־הָעֵ֛ץ"}     | ${"κολ αωλάμ κολ-αέτς"}
+`("General Rules:", ({ hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${hebrew} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description                   | hebrew           | transliteration
+  ${"1 syl, no accent"}         | ${"לָ֔ךְ"}       | ${"λαχ"}
+  ${"2 syls, accent on first"}  | ${"לֶ֬חֶם"}      | ${"λέχεμ"}
+  ${"2 syls, accent on last"}   | ${"דָּבָ֑ר"}     | ${"νταβάρ"}
+  ${"3 syls, accent on last"}   | ${"אֲרַנֵּ֥ן"}   | ${"αρανέν"}
+  ${"3 syls, accent on second"} | ${"הִגַּ֣דְתָּ"} | ${"ιγκάδτα"}
+`("Accents:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description           | hebrew         | transliteration
+  ${"vocal sheva"}      | ${"סְלִק"}     | ${"σελίκ"}
+  ${"silent sheva"}     | ${"סַלְכָה"}   | ${"σαλχά"}
+  ${"final sheva"}      | ${"כָּךְ"}     | ${"καχ"}
+  ${"two final shevas"} | ${"קָטַלְתְּ"} | ${"κατάλτ"}
+`("Shevas:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description                              | hebrew       | transliteration
+  ${"consonantal vav"}                     | ${"וָו"}     | ${"βαβ"}
+  ${"vav haser (holem precedes vav)"}      | ${"שָׁלֹום"}  | ${"σσαλώμ"}
+  ${"final holem vav"}                     | ${"כְּמוֹ"}  | ${"κεμώ"}
+  ${"shureq"}                              | ${"קוּם"}    | ${"κουμ"}
+  ${"initial shureq"}                      | ${"וּלֶחֶם"} | ${"ουλεχέμ"}
+  ${"Consonantal vav with holem as vowel"} | ${"עָוֹן"}   | ${"αβών"}
+`("Vavs:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description                              | hebrew         | transliteration
+  ${"consonantal yod"}                     | ${"יָם"}       | ${"γιαμ"}
+  ${"consonantal yod with hiriq as vowel"} | ${"יַיִן"}     | ${"γιαγίν"}
+  ${"hiriq followed by consonantal yod"}   | ${"סִיֵּם"}    | ${"σιγίεμ"}
+  ${"patach yod"}                          | ${"דְּרָכַי֙"} | ${"ντεραχάη"}
+`("Consonantal Yod:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+// For "hiriq yod: final with maqqef", it's unclear if the original input was correct.
+// Removed the final accent from ή
+describe.each`
+  description                       | hebrew             | transliteration
+  ${"hiriq yod: medial"}            | ${"אִירָ֑א"}       | ${"ιρά"}
+  ${"hiriq yod: final"}             | ${"אֲנִי"}         | ${"ανή"}
+  ${"hiriq yod: plural marker"}     | ${"דְּבָרִים"}     | ${"ντεβαρείμ"}
+  ${"hiriq yod: final with maqqef"} | ${"וַֽיְהִי־כֵֽן"} | ${"βαγιεη-χεν"}
+`("Hiriq Yod:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description           | hebrew       | transliteration
+  ${"medial tsere-yod"} | ${"אֵין"}    | ${"εν"}
+  ${"final tsere-yod"}  | ${"רִגְעֵי"} | ${"ριγαί"}
+`("Tsere Yod:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
+
+describe.each`
+  description                       | hebrew         | transliteration
+  ${"seghol-yod (2ms plural sufx)"} | ${"אֱלֹהֶיךָ"} | ${"ελωεχά"}
+`("Seghol Yod:", ({ description, hebrew, transliteration }) => {
+  const transliteratedHeb = transliterate(hebrew, romaniote);
+  test(`${description} to equal: ${transliteration}`, () => {
+    expect(transliteratedHeb).toEqual(transliteration);
+  });
+});
