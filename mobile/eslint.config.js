af1aa5fa0ebd39fb9e5ab15315c5dea0ba938c0f
Jhonny <edy602425@gmail.com>
2026-03-27 12:40:48 -0300

    solving lint problems and dependencies

--- mobile/eslint.config.js
+++ mobile/eslint.config.js
@@ -1,19 +1,10 @@
-import tseslint from "typescript-eslint";
-import importPlugin from "eslint-plugin-import";
 import { defineConfig } from "eslint/config";
 import expoConfig from "eslint-config-expo/flat.js";
 // https://docs.expo.dev/guides/using-eslint/
 
 export default defineConfig([
   expoConfig,
-  ...tseslint.configs.recommended,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
   {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      parser: tseslint.parser,
-    },
     settings: {
       "import/resolver": {
         node: {
