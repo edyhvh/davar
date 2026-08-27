c2dea915c8a6d5a416693a65306cd04b9b7e546c
Jhonny <60485595+jhonnyisaacc@users.noreply.github.com>
2026-08-27 12:33:13 -0300

    fix: restore generateDictionary H3068 overlay without truncating the generator

--- scripts/generate-static-data/index.ts
+++ scripts/generate-static-data/index.ts
@@ -1,1 +1,14 @@
-PLACEHOLDER_LOAD_FROM_FILE
+/// <reference path="../../web/node_modules/@types/node/index.d.ts" />
+
+import { createHash } from "crypto";
+import { mkdir, readdir, readFile, rm, writeFile } from "fs/promises";
+import { extname, join } from "path";
+import {
+  BUNDLE_VERSIONS,
+  CANONICAL_BOOK_ORDER,
+  DATA_ROOT,
+  DELITZSCH_TO_ENGLISH,
+  OE_TO_ENGLISH,
+  WEB_PUBLIC_DATA_ROOT,
+} from "./config";
+import { VERSIFICATION_DATA } from "../../shared/versificationData";
